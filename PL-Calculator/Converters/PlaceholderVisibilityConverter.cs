b62568ea31e579be9a6b9e80285f354adb2d1b0c
viktor <vkt.knnnk@gmail.com>
2024-02-05 12:46:37 +0500

    added decoration converters

---
+++ PL-Calculator/Converters/PlaceholderVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PL_Calculator.Converters
+{
+    public class PlaceholderVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string input = value as string;
+            if (!String.IsNullOrEmpty(input))
+                return Visibility.Hidden;
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Visibility.Hidden;
+        }
+    }
+}
---
+++ PL-Calculator/Converters/TestPointVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PL_Calculator.Converters
+{
+    public class TestPointVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string inputX = values[0] as string;
+            string inputY = values[1] as string;
+            if (!String.IsNullOrEmpty(inputX) && !String.IsNullOrEmpty(inputY))
+                return Visibility.Visible;
+            return Visibility.Hidden;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
